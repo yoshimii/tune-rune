f3d8480353a49c925d0dea868b19adc9e84f5cbc
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-09-09 15:35:25 -0500

    create data file set state to tracks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import './App.css';
+import tracks from './data';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+constructor() {
+  super();
+  this.state = {
+    trackList: tracks,
+    selectedTrack: null,
+  }
+}
+  render() {
+
+   return(
+   <div className="App">
       <header className="App-header">
-        
+        <h1>Tune Rune</h1>
+        <audio />
       </header>
     </div>
-  );
+   )};
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,6 @@
+const tracks = [
+    { id: 1, title: "Track 1" },
+    {id: 2, title: "Track 2"}
+]
+
+export default tracks;
